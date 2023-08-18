3ab9aa755a39f89cc58fc7a988eb48d326c6c371
Diego Mero <diegomero_1999@hotmail.com>
2023-08-18 07:01:24 -0500

    Add home view and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "pets#index"
+  root to: "home#index"
 
   resources :users
-
-  resources :pets, only: [:index]
 end
